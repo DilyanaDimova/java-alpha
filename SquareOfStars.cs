4b66c9eb3f9daab543d2d2feb8928abca318c528
Dilyana Dimova <dilqna9@abv.bg>
2016-06-09 09:21:14 +0300

    First-Steps-in-Coding

---
+++ SquareOfStars.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            var n = int.Parse(Console.ReadLine());
+            char edge = '*';
+            
+             for (int row = 0; row < n; row++)
+             {
+                  for (int col = 0; col < n; col++)
+                  {
+                      if (col == 0 || row == 0 || row == n-1 || col == n-1)
+                      { 
+                          Console.Write(edge);
+                      }
+                      else
+                      {
+                      Console.Write(" ");
+                      }
+                 }
+                 Console.WriteLine();
+             }
+         }
+    }
+}
