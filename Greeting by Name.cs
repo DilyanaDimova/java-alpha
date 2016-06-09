9af97509014efac682bbe504b0328f82bd001936
Dilyana Dimova <dilqna9@abv.bg>
2016-06-09 09:54:36 +0300

    Simple-Calculations

---
+++ Greeting by Name.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GreetingName
+{
+    public class StartUp
+    {
+        public static void Main()
+        {    
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
